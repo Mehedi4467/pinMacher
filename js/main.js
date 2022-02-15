0a23c6bd6fd634fd110b4d945e7d30817233285e
Mehedi4467 <mehedihassan4467@gmail.comm>
2022-02-16 00:13:47 +0600

    final commit

--- js/main.js
+++ js/main.js
@@ -14,9 +14,10 @@ function pinValidition(){
 // pin generate button 
 
 document.getElementById('generate-button').addEventListener('click', function(){
-    const pinShow = document.getElementById('pin-show');
+   const pinShow = document.getElementById('pin-show');
    const pin = pinValidition();
    pinShow.value = pin;
+
 });
 
 
@@ -36,7 +37,42 @@ document.getElementById('key-pad').addEventListener('click', function(event){
         keyShow.value = currentNumber;
        
     }
+  
+});
 
-    
-    
+
+// verify pin function
+function verifyPin(){
+    const currentPin = document.getElementById('pin-show').value;
+    const keyShow = document.getElementById('key-show').value;
+    const pinMach = document.getElementById('mach-pin');
+    const notMach = document.getElementById('not-mach');
+    if(currentPin == keyShow){
+        
+        pinMach.style.display = 'block';
+        notMach.style.display = 'none';
+        document.getElementById('massage').style.display = 'none';
+    }
+    else{
+
+        notMach.style.display = 'block';
+        pinMach.style.display = 'none';
+        document.getElementById('massage').style.display = 'block';
+
+        let machCountText = document.getElementById('machCount');
+        let machCount = parseInt(machCountText.innerText) - 1;
+        if (machCount < 1) {
+            const pinShow = document.getElementById('pin-show');
+            document.getElementById('massage').innerText = 'Set the new PIN again';
+            pinShow.value = '';
+            window.location.reload();
+        }
+        else{
+            machCountText.innerText = machCount;
+        }
+    }
+}
+
+document.getElementById('submit-button').addEventListener('click', function(){
+    verifyPin();
 })
